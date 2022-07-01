dbb0db93387e948b2d868adf8603cf0471048a6e
m-ghiringhelli <mghiring@gmail.com>
2022-07-01 14:32:49 -0700

    added getbyemail method to user class so userservice can access user object to create cookie

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -9,13 +9,18 @@ const mockUser = {
   password: '12345'
 };
 
+const mockUser2 = {
+  email: 'test2@example.com',
+  password: '12345'
+};
+
 describe('user sign in and sign up routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   it('creates a new user', async () => {
-    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app).post('/api/v1/users/sessions').send(mockUser);
     const { email } = mockUser;
     expect(res.body).toEqual({
       id: expect.any(String),
@@ -23,15 +28,16 @@ describe('user sign in and sign up routes', () => {
     });
   });
 
-  it('should delete a user', async () => {
+  it('delete route logs out a user', async () => {
     // add user
-    const addRes = await request(app).post('/api/v1/users').send(mockUser);
+    const addRes = await request(app).post('/api/v1/users').send(mockUser2);
+    console.log('addRes', addRes.body);
     expect(addRes.status).toBe(200);
 
     // delete user
-    const deleteRes = await request(app).delete('/api/v1/users');
+    const deleteRes = await request(app).delete('/api/v1/users/5');
+    console.log('deleteRes', deleteRes.body);
     expect(deleteRes.status).toBe(200);
-
   });
 
   afterAll(() => {
